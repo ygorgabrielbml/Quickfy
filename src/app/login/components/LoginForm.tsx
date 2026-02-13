efa57d9530b57db4f22c90fbc9bd611b43f9c0ae
Ygor <ygorgabrielbml@gmail.com>
2026-02-13 09:27:22 -0300

    feat: loginForm response but needs refactor

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -1,12 +1,33 @@
 import { RememberMe } from "./RememberMe";
 import { Input } from "../../components/AuthInput";
 import { AuthButton } from "../../components/AuthButton";
+import { useState } from 'react';
 
-export function LoginForm(){
+export function LoginForm({ onSubmit }){
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const handleSubmit = async (e: Event) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3000/api/login", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({username, password})
+    });
+
+    const result = await response.json();
+    if (result.success) {
+      onSubmit(result);
+    } else {
+      alert ("Invalid Crendentials")
+    }
+  }
+
   return (
     <>
       {/* Form */}
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <Input type="text" placeholder="Username" />
         <Input type="password" placeholder="Password" />
 
