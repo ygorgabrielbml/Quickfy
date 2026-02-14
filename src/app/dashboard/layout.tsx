8b2ee8de980817895af84b9f31e5a26186815dbb
ygor <ygorgabrielbml@gmail.com>
2026-02-14 14:59:43 -0300

    refactor: new style and simple folders

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./components/Sidebar";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-800 via-gray-900 to-indigo-900">
+    <div className="min-h-screen bg-linear-to-br from-slate-800 via-gray-900 to-indigo-900">
       <Navbar />
       <div className="flex">
         <Sidebar />
